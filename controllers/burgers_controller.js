4f96ab453654166a01be32f521231d1c811728e3
Adam SOudani <asoudani@email.com>
2020-05-13 14:51:32 -0700

    update controller router post and get

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -11,7 +11,19 @@ router.get('/', function(req, res){
         console.log(burgerObject);
         res.render('index', burgerObject);
     });
-    
+   
 });
+
+router.post('/api/burgers', function(req, res){
+    burger.insertOne([
+        'burger_name', 'devoured'
+    ],
+    [
+        req.body.name, req.body.devoured
+    ], function(result){
+        res.json({id: result.indertId });
+    }
+    )
+})
 
 module.exports = router;
