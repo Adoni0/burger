88f733d07f0366bff52c5366efb7401d3f66cb1c
Adam SOudani <asoudani@email.com>
2020-05-13 16:26:22 -0700

    controller router put

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -24,6 +24,22 @@ router.post('/api/burgers', function(req, res){
         res.json({id: result.indertId });
     }
     )
-})
+});
+
+router.put('/api/burgers/:id', function(req, res){
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+    burger.updateOne({
+        devoured: req.body.devoured
+    }), condition, function(result){
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+          } else {
+            res.status(200).end();
+          }
+    }
+});
 
 module.exports = router;
