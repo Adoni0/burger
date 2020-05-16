ae3ed6892b606628104274bfbf257cf41aed90b6
Adam SOudani <asoudani@email.com>
2020-05-16 13:56:58 -0700

    added jaws connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    port: 3306,
-    password: '',
-    database: 'burgers_db'
-});
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        port: 3306,
+        password: '',
+        database: 'burgers_db'
+    });
+};
+
 
 connection.connect(function(err, data){
     if(err) {
